44be1a6ae452f238618670bd98516793fb2d19c3
alu21901 <60663209+alu21901@users.noreply.github.com>
2020-10-20 00:47:38 +0100

    Add files via upload

---
+++ SCRIPTS/retrete.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class retrete : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private int counterClicked = 0;
+    private int counterByFrame = 0;
+    private const int ID = 4;
+
+    void Start()
+    {
+        Debug.Log("Hello Object: " + gameObject.name + "\tID: " + ID);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counterByFrame++;
+        Debug.Log("Iteracion numero: " + counterByFrame + " del objeto " + gameObject.name);
+    }
+
+    // Funcion que imprime un mensaje cada vez que se clicka sobre el collider del gameObject
+    void OnMouseDown() {
+        counterClicked++;
+        Debug.Log("Ha pulsado en " + gameObject.name + " " + counterClicked + " veces.");
+    }
+}
